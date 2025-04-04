20f19508e85b98332cb55b6c6e186c08d75c3e53
phloiola <paulo.loiola25@gmail.com>
2025-04-04 09:49:32 -0300

    feat: Implementação do Popover de Ordens de Produção
    Implementa um popover para exibir detalhes das ordens de produção ao clicar no código da ferramenta.
    
    - Adiciona um fragmento de Popover (PopoverOrdemProd.fragment.xml) para exibir os detalhes da ordem de produção.
    - Implementa a função `onPopoverOrdemProd` no controller `List.controller.js` para abrir o popover e filtrar os dados da ordem de produção com base na ferramenta, data de programação, comprimento e têmpera da linha.
    - Adiciona a função `onClosePopoverOrdemProd` para fechar o popover.
    - Adiciona textos para o fragmento no arquivo i18n.properties.
    - Altera a coluna de Ferramenta na tabela principal para implementar a ação de clique para abrir o popover.

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
 ],
-function (Controller) {
+function (Controller, Filter, FilterOperator) {
     "use strict";
-
+    
     let vm = null;
 
     return Controller.extend("br.com.perfilaluminio.zppapontreprf.controller.List", {
@@ -23,11 +25,61 @@ function (Controller) {
         },
 
         onBeforeRebindTable: function (oEvent) {
-            // let oTable = oEvent.getSource();
-            // let oModel = oTable.getModel();
 
-            // var mBindingParams = oEvent.getParameter("bindingParams");
-            // mBindingParams.parameters["expand"] = "OP";
+        },
+        onPopoverOrdemProd: function (oEvent) {
+                
+            // Adquire referencia do campo que o usuário clicou
+            let oSource = oEvent.getSource();
+
+            // Adquire dados da linha que foi feito o clique
+            let oDataLine = oSource.getBindingContext().getObject();
+
+            // create popover
+            if (!vm._pPopover) {
+                vm._pPopover = sap.ui.core.Fragment.load({
+                    id: vm.getView().getId(),
+                    name: "br.com.perfilaluminio.zppapontreprf.view.fragment.PopoverOrdemProd",
+                    controller: this
+                }).then(function(oPopover){
+                    vm.getView().addDependent(oPopover);
+                    return oPopover;
+                });
+            }
+
+            vm._pPopover.then(function(oPopover){
+                
+                vm.oPopoverOrdemProd = oPopover;
+
+                let sPath =  "/OrdemProducaoSet";
+                let aFilters = [];
+
+                    var oFilter = new Filter("Equnr", FilterOperator.EQ, oDataLine.Equnr);
+                    aFilters.push(oFilter);
+                    oFilter = new Filter("DtProgr", FilterOperator.EQ, new Date( oDataLine.DtProgr ));
+                    aFilters.push(oFilter);
+                    oFilter = new Filter("Comprimento", FilterOperator.EQ, oDataLine.Comprimento);
+                    aFilters.push(oFilter);
+                    oFilter = new Filter("Tempera", FilterOperator.EQ, oDataLine.Tempera);
+                    aFilters.push(oFilter);
+
+                    oFilter = new Filter("CentroTrab", FilterOperator.EQ, oDataLine.CentroTrab );
+                    aFilters.push(oFilter);
+
+                vm.oView.byId("idPopoverOrdemProdTable").bindItems({
+                    // path: "MainOData>/OrdemProducaoSet",
+                    path: sPath,
+                    filters: aFilters,
+                    templateShareable: true,
+                    template: vm.oView.byId("idPopoverOrdemProdColumnListItem")
+                } );
+
+                oPopover.openBy(oSource);
+            });
+
+        },
+        onClosePopoverOrdemProd: function(oEvent) {
+            vm.oPopoverOrdemProd.close();
         }
     });
 });
